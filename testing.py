da428c94f4d42179d8a6021bb1fde8148135599b
Kevin Dieny <kdieny@callsource.com>
2020-09-25 11:58:06 -0700

    Built to print html contents

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
 from urllib.request import urlopen
 url = "https://www.cnn.com/2020/09/25/politics/census-counting-timeline-federal-judge/index.html"
 page = urlopen(url)
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+print(html)
